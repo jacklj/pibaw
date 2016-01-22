0f1f61f9fb7b86057abebc16cd2c50a4a6107a93
Jack Lawrence-Jones <jackvlj@gmail.com>
2016-01-22 00:55:44 +0000

    changed production settings

--- pibaw/settings/production.py
+++ pibaw/settings/production.py
@@ -2,6 +2,10 @@ from .base import *
 
 
 DEBUG = False
+ALLOWED_HOSTS = ['jackvlj.pythonanywhere.com']
+
+SECRET_KEY = 'n(1w5+i^7nn&bu_5rz^u=1&+xn^c3jml(@ff()r+q8qyh%d1@d'
+
 
 try:
     from .local import *
