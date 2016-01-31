d50ac614dee8f1a5928f751d2cbe8fe3740609c2
Jack Lawrence-Jones <jackvlj@gmail.com>
2016-01-31 21:25:38 +0000

    updated settings

--- pibaw/settings/production.py
+++ pibaw/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 DEBUG = False
-ALLOWED_HOSTS = ['jackvlj.pythonanywhere.com']
+ALLOWED_HOSTS = ['jackvlj.pythonanywhere.com', 'www.picturesinblackandwhite.com', 'picturesinblackandwhite.com', 'www.picturesinblackandwhite.co.uk', 'picturesinblackandwhite.co.uk']
 
 SECRET_KEY = 'n(1w5+i^7nn&bu_5rz^u=1&+xn^c3jml(@ff()r+q8qyh%d1@d'
 
